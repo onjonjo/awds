b7853ba1488b881957f72b8006d01556cfe2cd29
onjo <onjo@d2c7b242-0724-0410-acbd-db3348fdb001>
2006-12-08 10:21:11 +0000

    initial import
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/awds/routing@19 d2c7b242-0724-0410-acbd-db3348fdb001

---
+++ src/TopoPacket.cc
@@ -0,0 +1,111 @@
+
+
+#include <algorithm>
+
+#include <awds/TopoPacket.h>
+#include <awds/routing.h>
+#include <madwifi/RateMonitor.h>
+
+using namespace std;
+
+void TopoPacket::setNeigh(AwdsRouting *interf, gea::AbsTime t) {
+  
+    unsigned char  n = 0;
+	    
+    char *addr =  &(packet.buffer[OffsetLinks]);
+
+    RateMonitor *rm = interf->madwifiRateMonitor;
+    
+    if ( (interf->metrics == Routing::TransmitDurationMetrics) && (rm != 0) )
+	rm->update();
+    
+
+    for (int i = 0; i < interf->numNeigh; ++i) {
+	    
+	if ( interf->neighbors[i].isBidiGood(t, interf->myNodeId) )
+	    {
+		NodeId nId = interf->neighbors[i].id;
+		nId.toArray(addr); 
+		++n;
+		addr += NodeId::size;
+		int metric;
+		
+		switch (interf->metrics) {
+		
+		case Routing::PacketLossMetrics:
+		    metric = 0xff - (interf->neighbors[i].quality() - 1) * (0x100 / 32);    
+		    break;
+		
+		case Routing::EtxMetrics: 
+		    metric = 0xff - (interf->neighbors[i].quality() - 1) * (0x100 / 32);
+		    break;
+		
+		case Routing::TransmitDurationMetrics:
+		    {
+			assert(rm);
+			rm->update();
+			metric = rm->getTT(nId) / 256;
+		    }
+		    break;
+		case Routing::HopCountMetrics:
+		    metric = 0xff;
+		    break;
+		}
+		
+		// clamp ranges
+		metric = max( min(metric, 0xff), 1);
+		*addr = (char)metric;
+		assert(*addr);
+		addr += 1;
+
+	    }
+    }	
+    
+    packet.buffer[OffsetNumLinks] = (char)n;
+    packet.size = OffsetLinks + (NodeId::size + 1) * (size_t)(n);
+    
+    //  assert(getNumLinks() == n);
+}   
+
+/* magic cookie */
+const char *magic = "\t\r\naw"/*""*/"ds routi"
+    "n"/*"Hello Welt's"*/"g "
+    /**/"©""2""0""0""6 by A";
+const char *magic2 = "ndr"
+    "é"
+    " "
+    "H" /*0x100, 0x23, 0x42, 0x54, 0x00*/
+    "e""r""m""s ";
+
+#include <iostream>
+
+using namespace std;
+
+void TopoPacket::print() {
+
+    ostream& os = GEA.dbg();
+    
+    os << "packet[" << packet.size << "] " << getSrc() << " : ";
+    
+    int n = getNumLinks();
+    
+    char *addr =  &(packet.buffer[OffsetLinks]);
+    
+    NodeId id;
+    for (int i = 1 ; i < n; ++i) {
+	
+	id.fromArray(addr);
+	os << id << " ";
+	addr += NodeId::size + 1;
+    }
+    
+    os << endl;
+}
+
+
+/* This stuff is for emacs
+ * Local variables:
+ * mode:c++
+ * c-basic-offset: 4
+ * End:
+ */
