235c23e030b96dc6de1bf327e72cba6b11fbbc28
jenz <jenz@d2c7b242-0724-0410-acbd-db3348fdb001>
2007-10-04 09:18:58 +0000

    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/awds/routing@177 d2c7b242-0724-0410-acbd-db3348fdb001

---
+++ src/gea2mad.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <gea/ObjRepository.h>
+#include <gea/Blocker.h>
+#include <gea/API.h>
+
+#include <awds/gea2mad.h>
+
+#include <gea/ShadowEventHandler.h>
+#include "common/scheduler.h"
+
+#include <dlfcn.h>
+
+//extern Tcl &_ZN3Tcl9instance_E();
+
+double awds::gea2mad::getTT(int id) {
+  Tcl &tcl = Tcl::instance(); //_ZN3Tcl9instance_E();
+  char buf[100];
+  sprintf(buf,"%s getTransmitTime %i",shadow_currentNode->name(),id);
+  tcl.evalc(buf);
+  return atof(tcl.result());
+}
+
+extern "C"
+#ifdef PIC
+int gea_main(int argc, const char  * const * argv) 
+#else
+int awdsRouting_gea_main(int argc, const char  * const *argv) 
+#endif
+{
+  ObjRepository& rep = ObjRepository::instance();
+  awds::gea2mad *g2m(new awds::gea2mad);
+  REP_INSERT_OBJ(awds::gea2mad*,gea2mad,g2m);
+  return 0;
+}
