7e8ac9c72396079776b2919b34004dabc2c3f1a8
jenz <jenz@d2c7b242-0724-0410-acbd-db3348fdb001>
2007-10-04 09:18:21 +0000

    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/awds/routing@171 d2c7b242-0724-0410-acbd-db3348fdb001

---
+++ src/awds/TTMetric.h
@@ -0,0 +1,45 @@
+#ifndef D__TTMetric
+#define D__TTMetric
+
+#include <awds/UCastMetric.h>
+#include <map>
+
+namespace awds {
+  class gea2mad;
+  class TTMetric : public UCastMetric {
+  public:
+    struct NodeData {
+      int tt;
+      bool active;
+      gea::AbsTime lastsend;
+      
+      NodeData():tt(max_quality),active(true),lastsend(0) {
+      }
+    };
+    typedef std::map<NodeId,NodeData> TTData;
+    TTData ttData;
+    gea::Blocker blocker;
+    gea::Duration interval;
+
+    virtual void on_recv(BasePacket *p,gea::AbsTime t) {}
+    virtual void on_wait(gea::Handle *h,gea::AbsTime t);
+
+    bool debug;
+    unsigned int packetSize;
+
+    TTMetric(Routing *r);
+    virtual ~TTMetric();
+    void start();
+    gea2mad *g2m;
+    virtual int update();
+    virtual RTopology::link_quality_t my_get_quality(NodeDescr &ndescr);
+    virtual unsigned long my_calculate(RTopology::link_quality_t forward,RTopology::link_quality_t backward);
+    virtual std::string get_values();
+
+    virtual void addNode(NodeId &nodeId);
+    virtual void begin_update();
+    virtual void end_update();
+  };
+}
+
+#endif // D__TTMetric
