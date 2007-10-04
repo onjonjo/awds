316003380ed2405eb05cf4a84ebe56fc92866bd6
jenz <jenz@d2c7b242-0724-0410-acbd-db3348fdb001>
2007-10-04 09:18:31 +0000

    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/awds/routing@173 d2c7b242-0724-0410-acbd-db3348fdb001

---
+++ src/awds/gea2mad.h
@@ -0,0 +1,14 @@
+#ifndef D__gea2mad
+#define D__gea2mad
+
+#include <string>
+
+
+namespace awds {
+  class gea2mad {
+  public:
+    virtual double getTT(int id);
+    virtual ~gea2mad() {}
+  };
+}
+#endif // D__gea2mad
